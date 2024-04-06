5497e95d0c22009c8a94ea8dc2cd56313c5413dd
ouwx <69306954+ouwx@users.noreply.github.com>
2024-04-06 08:53:02 +0000

    add tree

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <div>
-      <el-button @click="startHacking">Start</el-button>
-    </div>
-  </div>
+  <el-container>
+    <el-header>Header</el-header>
+    <el-container>
+      <el-aside width="200px">{{tree}}</el-aside>
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
+import {{tree}} from "pages/tree.vue";
 export default {
   methods: {
-    startHacking () {
+    startHacking() {
       this.$notify({
         title: 'It works!',
         type: 'success',
@@ -23,8 +25,38 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Helvetica, sans-serif;
+.el-header,
+.el-footer {
+  background-color: #B3C0D1;
+  color: #333;
   text-align: center;
+  line-height: 60px;
+}
+
+.el-aside {
+  background-color: #D3DCE6;
+  color: #333;
+  text-align: center;
+  line-height: 200px;
+}
+
+.el-main {
+  background-color: #E9EEF3;
+  color: #333;
+  text-align: center;
+  line-height: 160px;
+}
+
+body>.el-container {
+  margin-bottom: 40px;
+}
+
+.el-container:nth-child(5) .el-aside,
+.el-container:nth-child(6) .el-aside {
+  line-height: 260px;
+}
+
+.el-container:nth-child(7) .el-aside {
+  line-height: 320px;
 }
 </style>
